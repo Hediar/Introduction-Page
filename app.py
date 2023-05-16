ffe07f91d3908a66ee7a17063e82f52774a65e32
Seryeong Lee <srlimvp@gmail.com>
2023-05-16 19:46:28 +0900

    update and post

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 from pymongo import MongoClient
 
-client = MongoClient
+client = MongoClient(
+    )
+
 db = client.dbsparta
+
 
 Flask
 app = Flask(__name__)
@@ -15,18 +18,16 @@ def home():
     return render_template('index.html')
 
 
-
-
 @app.route("/intro", methods=["POST"])
 def introduce():
     # 변수 선언
-    img_receive = request.form.get('url_give', 'default.jpg')
-    name_receive = request.form.get('name_give', 'None')
-    age_receive = request.form.get('age_give', 'N/A')
-    mbti_receive = request.form.get('mbti_give', 'Unknown')
-    hobby_receive = request.form.get('hobby_give', 'None')
-    live_receive = request.form.get('address_give', 'Unknown')
-    comment_receive = request.form.get('comment_give', '')
+    img_receive = request.form.get('url_give')
+    name_receive = request.form.get('name_give')
+    age_receive = request.form.get('age_give')
+    mbti_receive = request.form.get('mbti_give')
+    hobby_receive = request.form.get('hobby_give')
+    live_receive = request.form.get('address_give')
+    comment_receive = request.form.get('comment_give')
 
     doc = {
             'name': name_receive,
@@ -39,44 +40,63 @@ def introduce():
         }
     
     # 작성일지 수정일지 판별
-    # div = db.intro.find({'name':name_receive}, {})
-    # if div['name'] == 'None': 
-        # 팀원 추가 수행
+    name_list = []
+ 
+    div = list(db.intro.find({}, {'_id': False, 'name': 1}))
+    for i in div:
+        name_list.append(i['name'])
+
+    for j in name_list: # db 내에 해당 이름이 있다면?
+        if j == name_receive:
+            if age_receive != '':
+                db.intro.update_one({'name':name_receive},
+                                {
+                                    '$set':{'age': age_receive},
+                                    })
+                return jsonify({'msg': '수정 완료!'})
+            
+            if img_receive != '':
+                db.intro.update_one({'name':name_receive},
+                                {
+                                    '$set':{'img': img_receive},
+                                    })
+                return jsonify({'msg': '수정 완료!'})
+            
+            if mbti_receive != '':
+                db.intro.update_one({'name':name_receive},
+                                    {
+                                        '$set':{'mbti': mbti_receive},
+                                        })
+                return jsonify({'msg': '수정 완료!'})
+            
+            if hobby_receive != '':
+                db.intro.update_one({'name':name_receive},
+                                    {
+                                        '$set':{'hobby': hobby_receive},
+                                        })
+                return jsonify({'msg': '수정 완료!'})
+            
+            if live_receive != '':
+                db.intro.update_one({'name':name_receive},
+                                    {
+                                        '$set':{'live': live_receive},
+                                        })
+                return jsonify({'msg': '수정 완료!'})
+            
+            if comment_receive != '':
+                db.intro.update_one({'name':name_receive},
+                                    {
+                                        '$set':{'comment': comment_receive}
+                                        })
+                return jsonify({'msg': '수정 완료!'})
+        else:
+            continue
+    # 팀원 추가 수행
     db.intro.insert_one(doc)
     return jsonify({'msg': '저장 완료!'})
-    # else:
-    #     # 수정 수행 
-    #     if age_receive != 'N/A':
-    #         db.intro.update_one({name_receive},
-    #                             {
-    #                                 '$set':{'age': age_receive},
-    #                                 })
-    #     if img_receive != 'default.jpg':
-    #         db.intro.update_one({name_receive},
-    #                             {
-    #                                 '$set':{'img': img_receive},
-    #                                 })
-    #     if mbti_receive != 'Unknown':
-    #         db.intro.update_one({name_receive},
-    #                             {
-    #                                 '$set':{'mbti': mbti_receive},
-    #                                 })
-    #     if hobby_receive != 'None':
-    #         db.intro.update_one({name_receive},
-    #                             {
-    #                                 '$set':{'hobby': hobby_receive},
-    #                                 })
-    #     if live_receive != 'Unknown':
-    #         db.intro.update_one({name_receive},
-    #                             {
-    #                                 '$set':{'live': live_receive},
-    #                                 })
-    #     if comment_receive != '':
-    #         db.intro.update_one({name_receive},
-    #                             {
-    #                                 '$set':{'comment': comment_receive}
-    #                                 })
-            
+        
+
+        
 
 @app.route("/intro", methods=["GET"])
 def intro_get():
